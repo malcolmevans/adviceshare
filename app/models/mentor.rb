3408176a58b2885b5c7cdad7b6eac0464e010daa
Your Name <your.email@example.com>
2013-11-06 13:39:13 -0500

    Use @Mentors instead of @EarlySignups

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,15 +1,12 @@
 class Mentor < MailForm::Base
-  #Biographical Info
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :education,      :validate => true
 
-  #Mentor work info
-  attribute :education,      :validate => true
+  attribute :expertise,      :validate => true
   attribute :linkedin,      :validate => true
+  attribute :education,      :validate => true
   attribute :job,      :validate => true
   attribute :jobresponsibilities,      :validate => true
-  attribute :expertise,      :validate => true
   attribute :preferences,      :validate => true
 
   attribute :message
@@ -19,8 +16,8 @@ class Mentor < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "Mentor Signup!",
-      :to => "malcolm@adviceshare.co",
+      :subject => "AdviceShare Mentor Signup!",
+      :to => "mentors@adviceshare.co",
       :from => %("#{name}" <#{email}>)
     }
   end
