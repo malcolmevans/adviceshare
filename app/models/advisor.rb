b9966fb6c0264ae69422fcee7e215dee2cc49dd7
Your Name <your.email@example.com>
2013-12-31 11:44:15 -0500

    Update site

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -1,19 +1,16 @@
 class Advisor < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-
-  attribute :expertise,      :validate => true
-  attribute :linkedin,      :validate => true
   attribute :education
-  attribute :job
-  attribute :Job_Responsibilities
-  attribute :preferences,      :validate => true
+  attribute :preferences
+  attribute :expertise
+  attribute :linkedin
 
-  attribute :message
   attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
+
   def headers
     {
       :subject => "AdviceShare Adviser Signup!",
