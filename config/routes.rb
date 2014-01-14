d2e1cf37f5e71e860dfad89e7450fc6ec309a163
Your Name <your.email@example.com>
2014-01-14 17:54:16 -0500

    Update for latest blog post

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ AdviceShare::Application.routes.draw do
     match '/blog/highly-coveted-professional-and-academic-positions',    to: 'blogposts#openingPost',    via: 'get'
     match '/blog',    to: 'blogposts#index',    via: 'get'
     match '/blog/Life-Hacks-for-Holiday-Season-Productivity',    to: 'blogposts#holidayProductivity',    via: 'get'
+    match '/blog/Selecting-Applicants-For-Interviews',    to: 'blogposts#selectingCandidates',    via: 'get'
 
   #Blog Templates
     match '/blog/homepagetemplate',    to: 'blogposts#blogPostHomePageTemplate',    via: 'get'
